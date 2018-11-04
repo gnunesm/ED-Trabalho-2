9769fb4b2ca363a82dd6633618bfae9509b07e3c
gnunesm <gabnunes2112@gmail.com>
2018-11-04 16:01:26 -0200

    adicionada função que cria árvore de huffman

--- compacta.c
+++ compacta.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "arvore.h"
+#include "lista.h"
 
-int main() {
+Arv *arv_huffman(char *filename) {
+    int pesos[256] = { 0 };
+
+    FILE *fp;
+    char c;
+    if((fp = fopen(filename, "rt")) == NULL) {
+        printf("Erro na abertura do arquivo %s\n", filename);
+        return NULL;
+    }
+    while ((c = fgetc(fp)) != EOF)
+        pesos[(int)c]++;
+    fclose(fp);
+
+    int maior_peso = 0;
+    int peso, i;
+    for (peso=pesos[i]; i<256; i++)
+        if(peso > maior_peso)
+            maior_peso = peso;
+
+    TipoLista *lista_arvores = InicializaLista();
+    int j;
+    for(i=1; i<=maior_peso; i++)
+        for(j=0; j<256; j++)
+            if(pesos[j] == i)
+                Insere(arv_cria((char)j, i, NULL, NULL), lista_arvores);
+
+    Arv *nova, *a1, *a2;
+    int novo_peso;
+    while(!unico(lista_arvores)) {
+        a1 = Pop(lista_arvores);
+        a2 = Pop(lista_arvores);
+        nova = arv_cria((char)0, get_peso(a1)+get_peso(a2), a1, a2);
+        Insere(nova, lista_arvores);
+    }
+    Libera(lista_arvores);
+    return nova;
+}
+
+int main(int argc, char** argv) {
     
+    if(argc < 2) {
+        printf("Erro: Nome do arquivo de entrada não especificado\n");
+        return 1;
+    }
+
+    Arv *huff = arv_huffman(argv[1]);
 
     return 0;
 }
