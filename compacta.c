56228585fe23bf36f850f54cb7fe5ed38ea4d95d
gnunesm <gabnunes2112@gmail.com>
2018-11-04 23:01:19 -0200

    escrita do arquivo de saida (versão completa)

--- compacta.c
+++ compacta.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "arvore.h"
 #include "lista.h"
 #include "bitmap.h"
+
+bitmap buffer;
 
 Arv *arv_huffman(char *filename) {
     int pesos[256] = { 0 };         // vetor de pesos (sugerido na Dica 1)
@@ -18,16 +21,15 @@ Arv *arv_huffman(char *filename) {
     while ((c = fgetc(fp)) != EOF)
         pesos[(int)c]++;
     fclose(fp);
-
+    pesos[7] = 1;                                 // caracter artificial de fim de arquivo
     // Cria a lista ordenada de nós com os caracteres
     TipoLista *lista_arvores = InicializaLista();
     for(int i=0; i<256; i++)
         if(pesos[i] != 0)
-            Insere_ordenado(arv_cria((char)i, pesos[i], NULL, NULL), lista_arvores);
+            Insere_ordenado(arv_cria((unsigned char)i, pesos[i], NULL, NULL), lista_arvores);
 
     // Junta os nós para formar a árvore de Huffman
     Arv *nova, *a1, *a2;
-    int novo_peso;
     while(!unico(lista_arvores)) {
         a1 = Pop(lista_arvores);
         a2 = Pop(lista_arvores);
@@ -40,9 +42,11 @@ Arv *arv_huffman(char *filename) {
 
 void preenche_tabela(bitmap *tabela, Arv *a, bitmap b) {
     if(!arv_vazia(a)) {
-        if(folha(a))
-            for (int i=0; i<bitmapGetLength(b); i++)
+        if(folha(a)) {
+            for (int i=0; i<bitmapGetLength(b); i++) {
 		        bitmapAppendLeastSignificantBit(tabela+info(a), bitmapGetBit(b, i));
+            }
+        }
         else {
             bitmap b2 = bitmapInit(8);
             for (int i=0; i<bitmapGetLength(b); i++)
@@ -58,8 +62,51 @@ void preenche_tabela(bitmap *tabela, Arv *a, bitmap b) {
     }
 }
 
+FILE *iniciar_escrita(char *filename) {
+    return fopen(filename, "w");
+}
+
+void escreve_bit(unsigned char bit, FILE *fp) {
+    printf("tenta\n");
+    bitmapAppendLeastSignificantBit(&buffer, bit);
+    printf("consegue\n");
+    if(bitmapGetLength(buffer) == 8) {
+        printf("aqui\n");
+        fprintf(fp, "%c", buffer.contents[0]);
+        buffer.contents[0] = 0x00;
+        buffer.length = 0;
+    }
+}
+
+void insere_cabecalho(FILE *fp, Arv *a) {
+    bitmap aux = bitmapInit(8);
+    aux.length = 8;
+    if(!arv_vazia(a)) {
+        if(folha(a)) {
+            escreve_bit(1, fp);
+            aux.contents[0] = info(a);
+            for(int i=0; i<8; i++)
+                escreve_bit(bitmapGetBit(aux, i), fp);
+        }
+        else {
+            escreve_bit(0, fp);
+            insere_cabecalho(fp, get_sae(a));
+            insere_cabecalho(fp, get_sad(a));
+        }
+    }
+}
+
+void encerrar_escrita(FILE *fp) {
+    int falta = 8 - bitmapGetLength(buffer);
+    for(int i=0; i<falta; i++)
+        escreve_bit(0, fp);
+    fclose(fp);
+}
+
 int main(int argc, char** argv) {
     
+    buffer = bitmapInit(8);
+
     if(argc < 2) {
         printf("Erro: Nome do arquivo de entrada não especificado\n");
         return 1;
@@ -67,11 +114,35 @@ int main(int argc, char** argv) {
 
     Arv *huff = arv_huffman(argv[1]);
 
+    arv_imprime(huff);
+
     bitmap tabela[256];
     bitmap aux = bitmapInit(8);
     for(int i=0; i<256; i++)
         tabela[i] = bitmapInit(8);
     preenche_tabela(tabela, huff, aux);
+
+    char *output_name = (char *)malloc(sizeof(char)*(strlen(argv[1])+2));
+    strcpy(output_name, argv[1]);
+    strcpy(output_name+strlen(argv[1])-3, "comp");
+
+    FILE *out = iniciar_escrita(output_name);
+
+    insere_cabecalho(out, huff);
+
+    FILE *in = fopen(argv[1], "rt");
+    char c;
+    
+    while ((c = fgetc(in)) != EOF) {
+        for(int i=0; i<bitmapGetLength(tabela[c]); i++)
+		    escreve_bit(bitmapGetBit(tabela[c], i), out);
+    }
+
+    for(int i=0; i<bitmapGetLength(tabela[254]); i++)
+		escreve_bit(bitmapGetBit(tabela[254], i), out);
+
+    encerrar_escrita(out);
+    fclose(in);
 
     return 0;
 }
